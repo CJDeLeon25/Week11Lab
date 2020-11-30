3a2f50e973c42bb7e2e53ebccda45c39ae6f3305
798676 <798676@SAIT224203IT.ACDM.DS.SAIT.CA>
2020-11-29 22:10:29 -0700

    set up week 11 demo

---
+++ src/java/utilities/CookieUtil.java
@@ -0,0 +1,27 @@
+
+package utilities;
+
+import javax.servlet.http.Cookie;
+
+/**
+ *
+ * @author 798676
+ */
+public class CookieUtil {
+    
+    public static String getCookieValue (Cookie[] cookies, String cookieName){
+       
+        String cookieValue = "";
+        
+        if(cookies != null){
+            for (Cookie cookie: cookies){
+                if(cookieName.equals(cookie.getName())){
+                    cookieValue = cookie.getValue();
+                }
+            }
+        }
+        return cookieValue;
+    
+    }
+    
+}
